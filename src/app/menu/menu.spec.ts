a5011bb6f12e821f24c523f38ca8591e3e79d8b4
sila-B00 <bahadirs311@gmail.com>
2026-05-27 21:43:40 +0300

    feat: menu yonetim paneli tasarimi ve standalone moduller tamamlandi

--- src/app/menu/menu.spec.ts
+++ src/app/menu/menu.spec.ts
@@ -1,22 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- Bunu ekleyin
+import { FormsModule } from '@angular/forms';     // <-- Bunu ekleyin
 
-import { Menu } from './menu';
-
-describe('Menu', () => {
-  let component: Menu;
-  let fixture: ComponentFixture<Menu>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Menu],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(Menu);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, FormsModule], // <-- Buraya bu iki modülü kesinlikle yazın!
+  templateUrl: './menu.html',
+  styleUrls: ['./menu.css']
+})
+export class MenuComponent {
+  // Mevcut kodlarınız (Category, selectedMenu, menus, vb.) aşağıda aynen kalsın...
+}
